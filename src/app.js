2716aeafd91ad0fc4555fad6ba543263dae55a64
MatiasSilvaAreal <matiherrera97@gmail.com>
2023-10-27 04:04:41 -0300

    html del login

--- src/app.js
+++ src/app.js
@@ -9,6 +9,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
+  });
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
+
+
+
+
+
+
+
+
