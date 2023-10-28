eabcb219daf3d84bb1f11bce59408ac6471bdecb
Daniel <dhanbrandan@gmail.com>
2023-10-28 03:57:02 -0300

    Se completo el archivo register html

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "login.html"));
   });
 
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "register.html"));
+  });  
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
