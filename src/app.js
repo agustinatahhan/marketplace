715900645741b775b70da64740752dde7518bdf8
agustinatahhan <agustahhan@gmail.com>
2023-10-29 17:10:32 -0300

    carrito compras & nav & footer

--- src/app.js
+++ src/app.js
@@ -11,20 +11,20 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "login.html"));
-  });
+});
 
 app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "register.html"));
-  });  
+});
+
+app.get("/cart", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "productCart.html"));
+});
+
+app.get("/detail", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "productDetail.html"));
+});
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
-
-
-
-
-
-
-
-
