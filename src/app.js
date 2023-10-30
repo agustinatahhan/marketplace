63e2fe4ed977b3f1269d100f344edafedaaa37ed
frantrouve <frantrouve@gmail.com>
2023-10-30 14:49:24 -0300

    carrousel

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const { trace } = require("console");
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -28,3 +29,36 @@ app.get("/detail", (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
+
+/* ------------------------CARROUSEL ARROWS----------------------------- */
+// function app() { }
+// window.onload = function (event) {
+//   var app = new app();
+//   window.app = app;
+// }
+// app.prototype.processingButton = function (event) {
+//   const btn = event.currentTarget;
+//   const carrouselList = recent.currentTarget.parentNode;
+//   const track = event.currentTarget.parentNode.querySelector('#track');
+//   const carrousel = track.querySelectorAll('.carrousel');
+
+//   const carrouselWidth = carrousel[0].offsetWidth;
+
+//   const trackWidth = track.offsetWidth;
+//   const listWidth = carrouselList.offsetWidth;
+
+//   track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+//   btn.dataset.button == "button-prev" ? prevAction(leftPosition, carrouselWidth, track) : nextAction(leftPosition, trackWidth, carrouselWidth, track);
+// }
+// let prevAction = (leftPosition, carrouselWidth, track) => {
+//   if (leftPsoition > 0) {
+//     track.style.left = `${-1 * (leftPosition - carrouselWidth)}px`;
+//   }
+// }
+
+// let nextAction = (leftPosition, trackWidth, listWidth, carrouselWidth, track) => {
+//   if (leftPosition < (trackWidth.listWidth)) {
+//     track.style.left = `${-1 * (leftPosition * carrouselWidth)}px`;
+//   }
+// }
+// /* ---------------------------------------------------------------------- */
