17bfcdf3b63800c28045976d19667528b805a493
agustinatahhan <agustahhan@gmail.com>
2023-11-27 15:37:43 -0300

    retromd

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,21 @@
+const controller = {
+    home: (req, res) => {
+      let juegos = [
+        {
+          id: 1,
+          nombre: 'Call of duty'
+        },
+        {
+          id: 2,
+          nombre: 'Overwatch'
+        },
+        {
+          id: 3,
+          nombre: 'Batman'
+        }
+      ]
+      res.render('home', { juegos })
+    }
+  }
+  
+  module.exports = controller;
