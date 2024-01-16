6189b3ecd065b834bae5c8d52004a9e8fbd50a26
ceciliamzr <ceciliamzr@gmail.com>
2024-01-15 23:57:04 -0300

    login funciona

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,11 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const bcryptjs = require("bcryptjs");
-const { log } = require("console");
-
-
 const { validationResult } = require('express-validator');
-
 const User = require('../models/User');
 
 const usersPath = path.join(__dirname, "../data/user.json");
@@ -21,11 +17,6 @@ module.exports = {
     
         if (userFound && userFound.password === password) {
             req.session.userLogged = userFound;
-            
-            // if (rememberme === 'on') {
-            //     res.cookie('rememberme', userFound.email, { maxAge: 60000 * 60 });
-            // }
-            
             res.redirect("/users/profile");
         } else {
             res.send("El password o el email es incorrecto");
@@ -35,44 +26,35 @@ module.exports = {
         res.render("users/profile.ejs", {user: req.session.userLogged});
     },
     processRegister: (req, res) => {
-    const resultValidation = validationResult(req);    
+        const resultValidation = validationResult(req);    
     
-    if (resultValidation.errors.length > 0){
-        
-        return res.render('users/register.ejs', {
-            errors: resultValidation.mapped(),
-            oldData: req.body
-        })
+        if (resultValidation.errors.length > 0){
+            return res.render('users/register.ejs', {
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
+        }
+
+        let userInDB = User.findByField('email', req.body.email);
+
+        if (userInDB){
+            return res.render('users/register.ejs', {
+                errors: {
+                    email: {
+                        msg: 'Este email ya esta registrado'
+                    }
+                },
+                oldData: req.body
+            });
+        }
+
+        let userToCreate = {
+            ...req.body,
+            password: bcryptjs.hashSync(req.body.password, 10),
+            image: req.file.filename
+        };
+
+        let UserCreated = User.create(userToCreate); 
+        return res.redirect('/users/login');
     }
-
-    let userInDB = User.findByField('email', req.body.email);
-
-    if (userInDB){
-        return res.render('users/register.ejs', {
-            errors: {
-                email: {
-                    msg: 'Este email ya esta registrado'
-                }
-            },
-            oldData: req.body
-        });
-    }
-
-    let userToCreate = {
-        ...req.body,
-        password: bcryptjs.hashSync(req.body.password, 10),
-        image: req.file.filename
-        res.render("users/profile", {user: req.session.userLogged});
-    }
-    let UserCreated = User.create(userToCreate); 
-    //console.log(req.body, req.file);
-     
-    //  return res.send('ok, se guardo el usuario'); 
-     return res.redirect('/users/login')
-   // return res.send('ok, pasaron las validaciones y no tienes errores');
-    //return res.send(resultValidation);    
-
-
-
-}
-}
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,70 +1,75 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path');
-const multer = require('multer');
-const {body} = require('express-validator');
+const path = require("path");
+const multer = require("multer");
+const { body } = require("express-validator");
 
 const storage = multer.diskStorage({
-    // destination: (req, file, cb)  =>{
-    //   cb(null, '../public/img/avatars');
-    // },
-    destination: (req, file, cb) => {
-        const absolutePath = path.join(__dirname, '../public/img/avatars');
-        cb(null, absolutePath);
-      },
-    filename: (req, file, cb) => {
-        // let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
-        // cb(null, fileName);
-        if (file) {
-            let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
-            cb(null, fileName);
-        } else {
-            cb(null, ''); // O proporciona un nombre de archivo predeterminado si lo deseas
-        }
+  // destination: (req, file, cb)  =>{
+  //   cb(null, '../public/img/avatars');
+  // },
+  destination: (req, file, cb) => {
+    const absolutePath = path.join(__dirname, "../public/img/avatars");
+    cb(null, absolutePath);
+  },
+  filename: (req, file, cb) => {
+    // let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
+    // cb(null, fileName);
+    if (file) {
+      let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
+      cb(null, fileName);
+    } else {
+      cb(null, ""); // O proporciona un nombre de archivo predeterminado si lo deseas
     }
-})
+  },
+});
 
-const uploadFile = multer ({storage});
-const {login, processLogin, profile, processRegister} = require("../controllers/users");
-const {login, processLogin, profile} = require("../controllers/users");
+const uploadFile = multer({ storage });
+const {
+  login,
+  processLogin,
+  profile,
+  processRegister,
+} = require("../controllers/users");
 const userLoggedMiddleware = require("../middleware/userLoggedMiddleware");
 
 const validations = [
-    body('firstName').notEmpty().withMessage('Tienes que escribir el nombre'),
-    body('lastName').notEmpty().withMessage('Tienes que escribir el apellido'),
-    body('email')
-    .notEmpty().withMessage('Tienes que escribir un email'),
-        //.notEmpty().withMessage('Tienes que escribir un email').bail(),
-        // .isEmail().withMessage('Debes escribir un formato de correo valido'),
-    
-    body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
-    body('image').custom((value, {req}) => {
-        let file = req.file;
-        let acceptedExtensions = ['.jpg', '.png', '.gif'];
+  body("firstName").notEmpty().withMessage("Tienes que escribir el nombre"),
+  body("lastName").notEmpty().withMessage("Tienes que escribir el apellido"),
+  body("email").notEmpty().withMessage("Tienes que escribir un email"),
+  //.notEmpty().withMessage('Tienes que escribir un email').bail(),
+  // .isEmail().withMessage('Debes escribir un formato de correo valido'),
 
-        if(file){
-            const fileExtension = path.extname(file.originalname);
-            if (!acceptedExtensions.includes(fileExtension)) {
-                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join()}`);
-            }
-        }else{
-            throw new Error('Tienes que subir una imagen');
-        }
+  body("password").notEmpty().withMessage("Tienes que escribir una contraseña"),
+  body("image").custom((value, { req }) => {
+    let file = req.file;
+    let acceptedExtensions = [".jpg", ".png", ".gif"];
 
-        
-
-        return true   
+    if (file) {
+      const fileExtension = path.extname(file.originalname);
+      if (!acceptedExtensions.includes(fileExtension)) {
+        throw new Error(
+          `Las extensiones de archivo permitidas son ${acceptedExtensions.join()}`
+        );
+      }
+    } else {
+      throw new Error("Tienes que subir una imagen");
     }
 
-    )
+    return true;
+  }),
 ];
 
 router.get("/login", login);
 router.post("/login", processLogin);
 
-router.post('/register', uploadFile.single('image'), validations,  processRegister);
+router.post(
+  "/register",
+  uploadFile.single("image"),
+  validations,
+  processRegister
+);
 
- router.get("/profile", userLoggedMiddleware, profile);
-
+router.get("/profile", userLoggedMiddleware, profile);
 
 module.exports = router;
